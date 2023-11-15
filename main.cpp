d33933800abcafc95b3392b61da494c358831f85
31persson <jperscas@myuax.com>
2023-11-16 00:39:19 +0100

    areas

--- main.cpp
+++ main.cpp
@@ -16,19 +16,19 @@ int main() {
 
     // Calcular y mostrar el área del triángulo
     double areaTriangulo = Geometria::calcularAreaTriangulo(baseTriangulo, alturaTriangulo);
-    std::cout << "El área del triángulo es: " << areaTriangulo << std::endl;
+    std::cout << "El area del triangulo es: " << areaTriangulo << std::endl;
 
     // Calcular y mostrar el área del círculo
     double areaCirculo = Geometria::calcularAreaCirculo(radioCirculo);
-    std::cout << "El área del círculo es: " << areaCirculo << std::endl;
+    std::cout << "El area del circulo es: " << areaCirculo << std::endl;
 
     // Calcular y mostrar el área del cuadrado
     double areaCuadrado = Geometria::calcularAreaCuadrado(ladoCuadrado);
-    std::cout << "El área del cuadrado es: " << areaCuadrado << std::endl;
+    std::cout << "El area del cuadrado es: " << areaCuadrado << std::endl;
 
     // Calcular y mostrar el área del rombo
     double areaRombo = Geometria::calcularAreaRombo(diagonalMayorRombo, diagonalMenorRombo);
-    std::cout << "El área del rombo es: " << areaRombo << std::endl;
+    std::cout << "El area del rombo es: " << areaRombo << std::endl;
 
     return 0;
 }
