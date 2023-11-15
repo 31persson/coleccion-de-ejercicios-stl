470ef3792105ab883cab152acf4526dd5788f70f
31persson <jperscas@myuax.com>
2023-11-16 00:46:51 +0100

    areas

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include "cuadrado.cpp"
 #include "rombo.cpp"
 
+// Agregar la inclusión del archivo circulo.h
+#include "circulo.h"
+
 int main() {
     // Uso de las funciones del espacio de nombres Geometria
     double baseTriangulo = 5.0;
@@ -16,19 +19,25 @@ int main() {
 
     // Calcular y mostrar el área del triángulo
     double areaTriangulo = Geometria::calcularAreaTriangulo(baseTriangulo, alturaTriangulo);
-    std::cout << "El area del triangulo es: " << areaTriangulo << std::endl;
+    std::cout << "El área del triángulo es: " << areaTriangulo << std::endl;
 
     // Calcular y mostrar el área del círculo
     double areaCirculo = Geometria::calcularAreaCirculo(radioCirculo);
-    std::cout << "El area del circulo es: " << areaCirculo << std::endl;
+    std::cout << "El área del círculo es: " << areaCirculo << std::endl;
 
     // Calcular y mostrar el área del cuadrado
     double areaCuadrado = Geometria::calcularAreaCuadrado(ladoCuadrado);
-    std::cout << "El area del cuadrado es: " << areaCuadrado << std::endl;
+    std::cout << "El área del cuadrado es: " << areaCuadrado << std::endl;
 
     // Calcular y mostrar el área del rombo
     double areaRombo = Geometria::calcularAreaRombo(diagonalMayorRombo, diagonalMenorRombo);
-    std::cout << "El area del rombo es: " << areaRombo << std::endl;
+    std::cout << "El área del rombo es: " << areaRombo << std::endl;
+
+    // Crear un objeto Circulo
+    Geometria::Circulo miCirculo(radioCirculo);
+
+    // Calcular y mostrar el perímetro del círculo
+    std::cout << "Perímetro del círculo: " << miCirculo.calcularPerimetro() << std::endl;
 
     return 0;
 }
